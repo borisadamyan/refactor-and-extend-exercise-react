95a8bfc221ca80c9ac3ef5aa32dbddedb95112ea
Boris <boris.adamyan@gmail.com>
2022-04-21 15:31:17 +0400

    refactor

--- src/App.js
+++ src/App.js
@@ -1,51 +1,39 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class App extends Component {
-  state = { state: '', year: '', isValid: undefined }
+  state = {state: '', year: '', isValid: undefined}
 
   isValidHome = (home) => {
-    if (isNaN(home.year)) { return false; }
 
-    if (home.year.length !== 2 && home.year.length !== 4) { return false; }
-
-    if (home.state.toLowerCase() === "new york") {
-      if (home.year.length === 2) {
-        if (parseInt(home.year) < 97 && parseInt(home.year) > 17) {
-          return false;
-        } else {
-          return true;
-        }
-      } else if (home.year.length === 4) {
-        if (parseInt(home.year) < 1997) {
-          return false;
-        } else {
-          return true;
-        }
-      } else {
-        return false;
-      }
-    } else if (home.state.toLowerCase() === "california") {
-      if (home.year.length === 2) {
-        if (parseInt(home.year) >= 6 && parseInt(home.year) <= 18) {
-          return false;
-        } else if (parseInt(home.year) <= 98) {
-          return false;
-        } else {
-          return true;
-        }
-      } else if (home.year.length === 4) {
-        if (parseInt(home.year) >= 2006) {
-          return false;
-        } else if (parseInt(home.year) <= 1998) {
-          return false;
-        } else {
-          return true;
-        }
-      } else {
-        return false;
-      }
-    } else {
+    if (isNaN(home.year)) {
       return false;
+    }
+
+    if (home.year.length !== 2 && home.year.length !== 4) {
+      return false;
+    }
+
+    switch (home.state.toLowerCase()) {
+
+      case 'new york':
+        if (home.year.length === 2) {
+          return parseInt(home.year) <= 17 || parseInt(home.year) >= 97;
+        }
+        if (home.year.length === 4) {
+          return parseInt(home.year) >= 1997;
+        }
+        return false;
+      case 'california':
+        if (home.year.length === 2) {
+          return parseInt(home.year) > 98;
+        }
+        if (home.year.length === 4) {
+          return parseInt(home.year) > 1998 && parseInt(home.year) < 2006;
+        }
+        return false;
+
+      default:
+        return false
     }
   };
 
@@ -55,14 +43,20 @@ class App extends Component {
         <h2>Home Approval System</h2>
         <h3>Current status: {this.state.isValid ? "Approved Home" : "Unapproved"}</h3>
         <span>Please enter your home information below.</span>
-        <br />
+        <br/>
         <label>State:</label>
-        <input type="text" name="state" onChange={(event) => this.setState({ ...this.state, state: event.target.value })} />
-        <br />
+        <input type="text" name="state"
+               onChange={(event) => this.setState({...this.state, state: event.target.value})}/>
+        <br/>
         <label>Year:</label>
-        <input type="text" name="year" onChange={(event) => this.setState({ ...this.state, year: event.target.value })} />
-        <br />
-        <button onClick={() => this.setState({ ...this.state, isValid: this.isValidCar({ state: this.state.state, year: this.state.year }) })}>Validate</button>
+        <input type="text" name="year" onChange={(event) => this.setState({...this.state, year: event.target.value})}/>
+        <br/>
+        {/*isValidCar function is not defined ----> isValidHome */}
+        <button onClick={() => this.setState({
+          ...this.state,
+          isValid: this.isValidHome({state: this.state.state, year: this.state.year})
+        })}>Validate
+        </button>
       </div>
     );
   }
